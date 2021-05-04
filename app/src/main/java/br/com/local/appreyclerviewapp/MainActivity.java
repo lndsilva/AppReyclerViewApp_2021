fe0c4237f88957d6ed88dee309cf55e37a4b0942
lndsilva <lndsilva@hotmail.com>
2021-05-04 15:43:07 -0300

    Initial commit

---
+++ app/src/main/java/br/com/local/appreyclerviewapp/MainActivity.java
@@ -0,0 +1,49 @@
+package br.com.local.appreyclerviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<Livros> lstLivros;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lstLivros = new ArrayList<>();
+
+        lstLivros.add(new Livros("Mindset", "Auto Ajuda", "A nova psicologia do sucesso", R.drawable.mindset));
+        lstLivros.add(new Livros("Orgulho e Preconceito - Jane Austen", "História", "Fala sobre preconceito", R.drawable.orgulh_preconceito));
+        lstLivros.add(new Livros("The Vegitarian ", "Categoria do livro", "Descrição do livro", R.drawable.thevigitarian));
+        lstLivros.add(new Livros("The Wild Robot", "Categoria do livro", "Descrição do livro", R.drawable.thewildrobot));
+        lstLivros.add(new Livros("The Martian", "Categoria do livro", "Descrição do livro", R.drawable.themartian));
+        lstLivros.add(new Livros("Privacy", "Categoria do livro", "Descrição do livro", R.drawable.privacy));
+        lstLivros.add(new Livros("Maria Semples", "Categoria do livro", "Descrição do livro", R.drawable.mariasemples));
+        lstLivros.add(new Livros("Hedied With", "Categoria do livro", "Descrição do livro", R.drawable.hediedwith));
+        lstLivros.add(new Livros("The Vegitarian ", "Categoria do livro", "Descrição do livro", R.drawable.thevigitarian));
+        lstLivros.add(new Livros("The Wild Robot", "Categoria do livro", "Descrição do livro", R.drawable.thewildrobot));
+        lstLivros.add(new Livros("The Martian", "Categoria do livro", "Descrição do livro", R.drawable.themartian));
+        lstLivros.add(new Livros("Privacy", "Categoria do livro", "Descrição do livro", R.drawable.privacy));
+        lstLivros.add(new Livros("Maria Semples", "Categoria do livro", "Descrição do livro", R.drawable.mariasemples));
+        lstLivros.add(new Livros("Hedied With", "Categoria do livro", "Descrição do livro", R.drawable.hediedwith));
+        lstLivros.add(new Livros("O sol é para todos", "Auto ajuda", "Um dos maiores clássicos da literatura mundial", R.drawable.osoleparatodos));
+
+
+        RecyclerView mRecyclerView = findViewById(R.id.id_recyclerView);
+
+        RecyclerViewAdapter mAdapter = new RecyclerViewAdapter(getApplicationContext(), lstLivros);
+
+        //Montando o layout para o modelo ser inserido
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(),3));
+
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
---
+++ app/src/main/java/br/com/local/appreyclerviewapp/RecyclerViewAdapter.java
@@ -0,0 +1,63 @@
+package br.com.local.appreyclerviewapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<Livros> mData;
+
+    public RecyclerViewAdapter(Context mContext, List<Livros> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater mInflater = LayoutInflater.from(mContext);
+        View view = mInflater.inflate(R.layout.cardview_item_livros, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.tvTituloLivro.setText(mData.get(position).getTitulo());
+        holder.imgLivro.setImageResource(mData.get(position).getMiniatura());
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTituloLivro;
+        ImageView imgLivro;
+        CardView cardView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvTituloLivro = itemView.findViewById(R.id.id_lblTituloLivro);
+            imgLivro = itemView.findViewById(R.id.id_imgLivro);
+            cardView = itemView.findViewById(R.id.idCardView);
+        }
+    }
+}
